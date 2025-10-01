f561720b51190e6c1001f110ffd4031a2d455cc3
yza2109 <yzadora.assuncao@escola.pr.gov.br>
2025-10-01 12:09:10 -0300

    upd

---
+++ exe/exe1.py/exe1.py
@@ -0,0 +1,17 @@
+# função get: método usado para acessar o valor em um dicionário
+preco_uva = loja_frutas.get('uva',{})
+print('uva:', preco_uva)
+
+# função update: método usado para atualizar os valores do dicionário
+loja_frutas.update({'uva': 8.90, 'tomate': 10})
+print(loja_frutas, '#tomate adicionado')
+
+loja_frutas.pop('tomate')
+print(loja_frutas, '#tomate removido')
+
+for chave, valor in loja_frutas.items():
+  print(f'key: {chave}: value: {valor}')
+
+
+
+
